ef00ef5094be8aeaecfb7e6829fab38cc59b6550
capybara1337 <imaevdanila@gmail.com>
2023-11-06 14:37:15 +0300

    Added BAR and LEADERBOARD

--- db_manager.py
+++ db_manager.py
@@ -1,6 +1,5 @@
 import sqlite3
 from admin import check_privilege
-from sys import maxsize
 def create(message):
     data = sqlite3.connect('baseddata.db')
     cur = data.cursor()
@@ -21,48 +20,70 @@ def create(message):
     data.commit()
     data.close()
 
+def buy_bar(message, name : str, bar : dict):
+    data = sqlite3.connect('baseddata.db')
+    cur = data.cursor()
+    id = message.chat.id
+    price = bar.get(name)
+    cur.execute("SELECT balance FROM users WHERE chatid = ?", (id,))
+    balance = cur.fetchone()[0]
+    print(balance)
+    print(price)
+    cur.execute("UPDATE users SET balance = ? WHERE chatid = ?", (balance - price, id))
+    data.commit()
+    data.close()
+
 def get_info(message):
     data = sqlite3.connect('baseddata.db')
     cur = data.cursor()
     id = message.chat.id
-    info = list(cur.execute("SELECT * FROM users where chatid = ?", (id,)))[0]
+    info = list(cur.execute("SELECT * FROM users WHERE chatid = ?", (id,)))[0]
     print(info)
     data.commit()
     data.close()
     return info
 
-def addnametodb(message, name):
+def addnametodb(message, name : list[str]):
     data = sqlite3.connect('baseddata.db')
     cur = data.cursor()
     id = message.chat.id
-    cur.execute("UPDATE users SET name = ? WHERE chatid = ?", (name, id))  
+    cur.execute("UPDATE users SET name = ?, surname = ? WHERE chatid = ?", (name[0], name[1], id))  
     data.commit()
     data.close()
 
-def addsurnametodb(message, surname):
-    data = sqlite3.connect('baseddata.db')
-    cur = data.cursor()
-    id = message.chat.id
-    cur.execute("UPDATE users SET surname = ? WHERE chatid = ?", (surname, id))  
-    data.commit()
-    data.close()
 def getleaderboard():
-    maxpositions = 8
+    maxpositions = 10
     data = sqlite3.connect('baseddata.db')
     cur = data.cursor()
-    cur.execute("SELECT surname, name FROM users WHERE isadmin = 0 ORDER BY balance DESC")
-    ls = cur.fetchmany(maxpositions)
+    cur.execute("SELECT COUNT(*) FROM users")
+    cur_positions = cur.fetchone()[0]
+    print(cur_positions)
+    cur.execute("SELECT surname, name, balance FROM users WHERE isadmin = 0 ORDER BY balance DESC")
+    if 0 < cur_positions <= maxpositions:
+        ls = cur.fetchmany(cur_positions)
+    elif cur_positions > maxpositions:
+        ls = cur.fetchmany(maxpositions)
+    else:
+        leaderboard = 'В базе данных нет данных'
+        return leaderboard
     leaderboard = ''
     try:
-        leaderboard += '1. 🥇 ' + str(ls[0][1]) + '\n'
-        leaderboard += '2. 🥈 ' + str(ls[1][1]) + '\n'
-        leaderboard += '3. 🥉 ' + str(ls[2][1])+ '\n'
-        count = 3
+        if cur_positions == 1:
+            leaderboard += '1. 🥇 ' + str(ls[0][0]) + ' ' + str(ls[0][1]) + ':   ' + str(ls[0][2]) + ' 💵 \n'
+            return leaderboard
+        elif cur_positions == 2:
+            leaderboard += '1. 🥇 ' + str(ls[0][0]) + ' ' + str(ls[0][1]) + ':   ' + str(ls[0][2]) + ' 💵 \n'
+            leaderboard += '2. 🥈 ' + str(ls[1][0]) + ' ' + str(ls[1][1]) + ':   ' + str(ls[1][2]) + ' 💵 \n'
+            return leaderboard
+        elif cur_positions >= 3:
+            leaderboard += '1. 🥇 ' + str(ls[0][0]) + ' ' + str(ls[0][1]) + ':   ' + str(ls[0][2]) + ' 💵 \n'
+            leaderboard += '2. 🥈 ' + str(ls[1][0]) + ' ' + str(ls[1][1]) + ':   ' + str(ls[1][2]) + ' 💵 \n'
+            leaderboard += '3. 🥉 ' + str(ls[2][0]) + ' ' + str(ls[2][1]) + ':   ' + str(ls[2][2]) + ' 💵 \n'
+            return leaderboard
         for i in range(3, maxpositions):
-            count+=1
-            leaderboard += str(count) +'. ' + str(ls[i][1]) + '\n'
+            leaderboard += str(i) +'. ' + str(ls[i][0]) + ' ' + str(ls[i][1]) + ':   ' + str(ls[i][2]) + ' 💵 \n'
     except IndexError:
         pass
-    return leaderboard
     data.commit()
     data.close()
+    return leaderboard
--- main.py
+++ main.py
@@ -2,47 +2,65 @@ import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import token
 import db_manager as db
-
+BANK = 10000
 bot = telebot.TeleBot(token)
+
+bar = {'Ягерьбомба' : 300, 'Водка' : 100}
 
 def main_menu(message: telebot.types.Message):
     markup = InlineKeyboardMarkup()
-    markup.row(InlineKeyboardButton('Взять деньги в долг💸💸', callback_data='lend_cash'))
+    markup.row(InlineKeyboardButton('Взять деньги в долг💸', callback_data='lend_cash'))
     markup.row(InlineKeyboardButton('Заказать напиток🍸', callback_data='bar'))
-    markup.add(InlineKeyboardButton('Игры🎰', callback_data='games'))
+    markup.add(InlineKeyboardButton('Перевод денег за границу💱', callback_data='transaction'))
+    markup.add(InlineKeyboardButton('Таблица лидеров🏅', callback_data='leaderboard'))
     bot.send_message(message.chat.id, f'Ваш баланс💰: {db.get_info(message)[3]} \nВыберете действие:', reply_markup=markup)
 
 @bot.message_handler(commands=['start'])
 def start(message: telebot.types.Message):
     db.create(message)
-    bot.send_message(message.chat.id, 'Привет, пожалуйста введи своё имя🥺: ')
+    bot.send_message(message.chat.id, 'привет, пожалуйста введи своё имя и фамилию, через пробел🥺 ')
     bot.register_next_step_handler(message, user_name)
     #main_menu(message)
 
 def user_name(message):
-    name = message.text
+    name = message.text.split(' ')
     db.addnametodb(message, name)
-    bot.send_message(message.chat.id, 'п-пожалуйста введи свою фамлилю🥵: ')
-    bot.register_next_step_handler(message, user_surname)
-
-def user_surname(message):
-    surname = message.text
-    db.addsurnametodb(message, surname)
     bot.send_message(message.chat.id, 'поздравляю! ты теперь официальный гость влада козлова🤩')
     main_menu(message)
+
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_start(callback: telebot.types.CallbackQuery):
-    # print(callback)
     bot.edit_message_reply_markup(chat_id=callback.message.chat.id, message_id=callback.message.id, reply_markup=None)
+    # print(callback)
     # bot.edit_message_text(text= ,chat_id=callback.message.chat.id, message_id=callback.message.id, reply_markup=)
     bot.send_message(callback.message.chat.id, 'тест')
     if callback.data == 'lend_cash':
-        bot.send_message(callback.message.chat.id, f'Ваш кредитный лимит: {1000 - db.get_info(callback.message)[2]} \nВведите сумму, которую хотите взять в долг:')
+        bot.send_message(callback.message.chat.id, 'Введите сумму, которую вы хотите взять в долг' + '\n' +
+                         'Комиссия составляет 3%, то есть взяв 100, придется вернуть 103')
+        bot.send_message(callback.message.chat.id, f'Ваш кредитный лимит: {1000 - db.get_info(callback.message)[3]} \nВведите сумму, которую хотите взять в долг:')
         bot.register_next_step_handler(callback.message, addlend)
     if callback.data == 'bar':
-        pass
-    if callback.data == 'games':
-        pass
+        count = 0
+        s = ''
+        for i in bar.items():
+            count+=1
+            s += str(count) + '. ' + i[0]+ ': ' + str(i[1]) + '\n'
+        bot.send_message(callback.message.chat.id, s + 'Напишите наименование напитка, который вы хотите заказать🍹:')
+        bot.register_next_step_handler(callback.message, bar_purchase)
+    if callback.data == 'canceled':
+        bot.send_message(callback.message.chat.id, 'Покупка отменена')
+        main_menu(callback.message)
+    if callback.data in bar.keys():
+        db.buy_bar(callback.message, callback.data, bar)
+        print(callback.data)
+        bot.send_message(callback.message.chat.id, 'Покупка прошла успешно! Заказ отправлен бармену ✅')
+        # Добавить отправку сообщения с заказом бармену :3
+        main_menu(callback.message)
+    if callback.data == 'leaderboard':
+        lb = db.getleaderboard()
+        bot.send_message(callback.message.chat.id, lb)
+        main_menu(callback.message)
+
 def addlend(message: telebot.types.Message):
     try:
         amount = int(message.text.strip())
@@ -53,4 +71,14 @@ def addlend(message: telebot.types.Message):
         bot.register_next_step_handler(message, addlend)
         return
     main_menu(message)
+
+def bar_purchase(message: telebot.types.Message):
+    id = message.chat.id
+    for i in bar.items():
+        if i[0] == message.text:
+            name = i[0]
+    markup = InlineKeyboardMarkup()
+    markup.row(InlineKeyboardButton('Купить🫰', callback_data=name))
+    markup.row(InlineKeyboardButton('Бросаю пить❤️‍🩹', callback_data='canceled'))
+    bot.send_message(message.chat.id, f'Ваш напиток : {message.text}', reply_markup=markup)  
 bot.polling()
